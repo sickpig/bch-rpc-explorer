85aed82dcae5b22050f26184bde229fe60a958b9
Georg Engelmann <mail@georg-engelmann.at>
2023-08-23 14:53:26 +0200

    Minor fixes (#195)

--- app/config.js
+++ app/config.js
@@ -182,7 +182,7 @@ module.exports = {
           title:"Related Sites",
           links:[
             // TODO: find better images for this URLs, in the mean time use bch.svg
-            {name: "Bitcoin Unlimited", url:"https://bitcoinunlimited.info", imgUrl:"/img/logo/bu.png"},
+            {name: "Bitcoin Unlimited", url:"https://www.bitcoinunlimited.info", imgUrl:"/img/logo/bu.png"},
             {name: "Bitcoin Cash Nodes", url:"https://cashnodes.io", imgUrl:"/img/logo/bch.svg"},
             {name: "BCH PoW Monitoring", url:"https://fork.lol", imgUrl:"/img/logo/fork.png"},
             {name: "BCH Specification", url:"https://reference.cash", imgUrl:"/img/logo/refcash.ico"},
@@ -202,7 +202,7 @@ module.exports = {
   donations:{
     addresses:{
       coins:["BCH"],
-      sites:{"BCH":"https://bitcoinunlimited.info"},
+      sites:{"BCH":"https://www.bitcoinunlimited.info"},
 
       "BCH":{address:"bitcoincash:pq6snv5fcx2fp6dlzg7s0m9zs8yqh74335tzvvfcmq"}
     }
